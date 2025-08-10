fab1a8a0c8299ee4866640feb51908435d3523e4
Mateusz Jakubowski <matebowski@gmail.com>
2025-08-10 23:01:48 +0200

    feat(products-page): add search & add-to-cart; switch to semantic locators

--- pages/ProductsPage.ts
+++ pages/ProductsPage.ts
@@ -1,1 +1,30 @@
-export class ProductsPage {}
+import { type Page, type Locator } from "@playwright/test";
+
+export class ProductsPage {
+  readonly page: Page;
+  readonly searchInput: Locator;
+  readonly searchButton: Locator;
+  readonly searchedProductsHeader: Locator;
+  readonly allProductsContainer: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.searchInput = page.locator("#search_product");
+    this.searchButton = page.locator("#submit_search");
+    this.searchedProductsHeader = page.getByRole("heading", {
+      name: "Searched Products",
+    });
+    this.allProductsContainer = page.locator(".features_items");
+  }
+
+  async searchForProduct(productName: string) {
+    await this.searchInput.fill(productName);
+    await this.searchButton.click();
+  }
+
+  getProductContainerByName(productName: string): Locator {
+    return this.allProductsContainer.locator(".product-image-wrapper", {
+      hasText: productName,
+    });
+  }
+}
